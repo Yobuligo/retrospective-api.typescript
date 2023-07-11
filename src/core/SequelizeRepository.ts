2d397d4a339a6b774e7bfb57f8f001371ab1130a
Peter <hoffmannpeter82@gmail.com>
2023-07-11 15:43:51 +0200

    Complete implementation of Sequelize

--- src/core/SequelizeRepository.ts
+++ src/core/SequelizeRepository.ts
@@ -1,4 +1,4 @@
-import { Model, ModelStatic } from "sequelize";
+import { Model, ModelStatic, WhereOptions } from "sequelize";
 import { IRepository } from "../shared/api/IRepository";
 import { IEntity } from "../shared/types/IEntity";
 import { IEntityDetails } from "../shared/types/IEntityDetails";
@@ -7,19 +7,29 @@ export class SequelizeRepository<T extends IEntity> implements IRepository<T> {
   constructor(private readonly model: ModelStatic<Model<T, IEntity>>) {}
 
   add(dataObject: IEntityDetails<T>): Promise<T> {
-    throw new Error("Method not implemented.");
+    return this.createPromise(async (resolve) => {
+      const data = await this.model.create(dataObject);
+      resolve(data.dataValues);
+    });
   }
 
   delete(dataObject: T): Promise<boolean> {
-    throw new Error("Method not implemented.");
+    return this.createPromise(async (resolve) => {
+      resolve(this.deleteById(dataObject.id));
+    });
   }
 
   deleteById(id: number): Promise<boolean> {
-    throw new Error("Method not implemented.");
+    return this.createPromise(async (resolve) => {
+      const data = await this.model.destroy({
+        where: { id: id } as WhereOptions,
+      });
+      resolve(data === 1);
+    });
   }
 
   findAll(): Promise<T[]> {
-    return new Promise(async (resolve) => {
+    return this.createPromise(async (resolve) => {
       const data = await this.model.findAll();
       const items = data.map((item) => item.dataValues);
       resolve(items);
@@ -27,7 +37,7 @@ export class SequelizeRepository<T extends IEntity> implements IRepository<T> {
   }
 
   findById(id: number): Promise<T | undefined> {
-    return new Promise(async (resolve) => {
+    return this.createPromise(async (resolve) => {
       const data = await this.model.findByPk(id);
       resolve(data?.dataValues ?? undefined);
     });
@@ -37,6 +47,28 @@ export class SequelizeRepository<T extends IEntity> implements IRepository<T> {
     id: number,
     dataObject: IEntityDetails<T>
   ): Promise<T | undefined> {
-    throw new Error("Method not implemented.");
+    return this.createPromise(async (resolve) => {
+      const data = await this.model.update(dataObject, {
+        where: { id: id } as WhereOptions,
+        returning: true,
+      });
+      if (data[0] === 1) {
+        resolve(data[1][0].dataValues);
+      } else {
+        resolve(undefined);
+      }
+    });
+  }
+
+  private createPromise<T>(
+    block: (resolve: (value: T | PromiseLike<T>) => void) => void
+  ): Promise<T> {
+    return new Promise(async (resolve, reject) => {
+      try {
+        await block(resolve);
+      } catch (error) {
+        reject(error);
+      }
+    });
   }
 }
